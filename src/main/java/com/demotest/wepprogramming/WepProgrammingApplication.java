ebab17fc3a9a593f814be777290bfafb06c008be
Hristijan Mijalkov <hristijan-mijalkov5@vista-point.io>
2024-10-18 18:33:56 +0200

    first commit

---
+++ src/main/java/com/demotest/wepprogramming/WepProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.demotest.wepprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WepProgrammingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WepProgrammingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demotest/wepprogramming/WepProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demotest.wepprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WepProgrammingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
